bd15e978afba399a7041c47d72e62f9bf2471aa1
tvduc <48077426+tvduc@users.noreply.github.com>
2019-02-28 20:11:40 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tinhtong
+namespace bai2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Ex1 x = new Ex1();
-            x.nhap();
-            x.In();
+            int n;
+            Console.WriteLine("nhap so doan tau:");
+            n = Int32.Parse(Console.ReadLine());
+            doantau[] mangdt = new doantau[100];
+            for (int i = 0; i < n; i++)
+            {
+                mangdt[i] = new doantau();
+                mangdt[i].nhap();
+                mangdt[i].intt();
+
+            }
+            doantau tau1 = new doantau();
+            tau1.nhap();
+            tau1.intt();
             Console.ReadLine();
         }
     }
+    
 }
---
+++ bai2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    class doantau
+    {
+        string matau;
+        int sokhach;
+        public void nhap()
+{
+            Console.WriteLine("nhap ma doan tau:");
+            this.matau = Console.ReadLine();
+            Console.WriteLine("nhap so khach:");
+            this.sokhach = Convert.ToInt32(Console.ReadLine());
+        }
+        public int tinhtien()
+        {
+            int tien;
+            if (this.sokhach < 100) tien = this.sokhach * 100000;
+            else tien = this.sokhach * 80000;
+            return tien;
+        }
+        public void intt()
+        {
+            Console.WriteLine("Doan tau co ma {0} co tien la {1}:", this.matau, this.tinhtien());
+
+        }
+    }
+}
